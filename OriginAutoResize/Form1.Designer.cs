4138c929af9eada9ae91a7bb681b232e3ea0a24c
Dendari92 <dendari@hotmail.it>
2014-09-16 06:53:28 +0200

    Initial Release

---
+++ OriginAutoResize/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OriginAutoResize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Proceed";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 46);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(185, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(57, 17);
+            this.toolStripStatusLabel1.Text = "Waiting...";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xml";
+            this.openFileDialog1.FileName = "local_";
+            this.openFileDialog1.Filter = "Local Profile|local_*|XML Files|*.xml";
+            this.openFileDialog1.RestoreDirectory = true;
+            this.openFileDialog1.Title = "Select your \'local_\' profile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 68);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Origin Auto Resize";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ OriginAutoResize/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace OriginAutoResize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Origin\\";
+            openFileDialog1.InitialDirectory = path;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) { 
+                String xmlfile = openFileDialog1.FileName;
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlfile);
+
+                IEnumerator ie = doc.SelectNodes("/Settings/Setting").GetEnumerator();
+
+                while (ie.MoveNext())
+                {
+                if ((ie.Current as XmlNode).Attributes["key"].Value == "AppSizePosition")
+                {
+                    (ie.Current as XmlNode).Attributes.RemoveAll();
+                }
+                else if ((ie.Current as XmlNode).Attributes["key"].Value == "ChatSizePosition")
+                {
+                    (ie.Current as XmlNode).Attributes.RemoveAll();
+                }
+                else if ((ie.Current as XmlNode).Attributes["key"].Value == "FriendsListSizePosition")
+                {
+                    (ie.Current as XmlNode).Attributes.RemoveAll();
+                }
+            }
+
+            doc.Save(xmlfile);
+
+            toolStripStatusLabel1.Text = "Done!";
+            System.Media.SystemSounds.Exclamation.Play();
+            }
+        }
+    }
+}
